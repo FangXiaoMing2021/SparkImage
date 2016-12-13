ac9eb3d3238f1611d3516670d5005a8e8d081760
fang <784945099@qq.com>
2016-12-13 16:41:47 +0800

    添加HBaseTest.scala

---
+++ src/main/scala/com/fang/spark/HBaseTest.scala
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// scalastyle:off println
+package com.fang.spark
+
+import org.apache.hadoop.hbase.client.ConnectionFactory
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.{HBaseConfiguration, HTableDescriptor, TableName}
+import org.apache.spark._
+
+object HBaseTest {
+  private[spark] val conf = HBaseConfiguration.create
+  def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("HBaseTest").setMaster("local[2]")
+    val sc = new SparkContext(sparkConf)
+    // please ensure HBASE_CONF_DIR is on classpath of spark driver
+    // e.g: set it through spark.driver.extraClassPath property
+    // in spark-defaults.conf or through --driver-class-path
+    // command line option of spark-submit
+   // val conf = HBaseConfiguration.create()
+    val tableName = "imagesTable"
+    // Other options for configuring scan behavior are available. More information available at
+    // http://hbase.apache.org/apidocs/org/apache/hadoop/hbase/mapreduce/TableInputFormat.html
+    conf.set("hbase.zookeeper.property.clientPort", "2181")
+    conf.set("hbase.zookeeper.quorum", "fang-ubuntu,fei-ubuntu,kun-ubuntu")
+    conf.set(TableInputFormat.INPUT_TABLE, tableName)
+    val connection = ConnectionFactory.createConnection(conf)
+    // Initialize hBase table if necessary
+    //val admin = new HBaseAdmin(conf)
+    val admin = connection.getAdmin()
+    if (!admin.isTableAvailable(TableName.valueOf(tableName))) {
+      val tableDesc = new HTableDescriptor(TableName.valueOf(tableName))
+      admin.createTable(tableDesc)
+    }
+    val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat],
+      classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
+      classOf[org.apache.hadoop.hbase.client.Result])
+    println(hBaseRDD.count())
+    sc.stop()
+    admin.close()
+  }
+}
+// scalastyle:on println
